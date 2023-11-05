48ee7a716b6c0bfa01486b3d51c9acd04bd0d912
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-11-05 17:20:36 +0100

    Change redirect url after google oauth

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ import os
 from google_auth_oauthlib.flow import Flow
 import threading
 
-from api import fetch_season_data, add_event_user_calendar, retrive_race_data, format_season_info
+from api import (
+    fetch_season_data,
+    add_event_user_calendar,
+    retrive_race_data,
+    format_season_info,
+)
 
 
 app = Flask("Cycling calendar")
@@ -17,10 +22,11 @@ GOOGLE_SECRET = os.getenv("GOOGLE_SECRET")
 
 # google set up
 flow = Flow.from_client_secrets_file(
-        'google_info/client_secrets.json',
-        scopes=['openid',  'https://www.googleapis.com/auth/calendar.events'],)
+    "google_info/client_secrets.json",
+    scopes=["openid", "https://www.googleapis.com/auth/calendar.events"],
+)
 
-flow.redirect_uri = 'https://127.0.0.1:5000'
+flow.redirect_uri = "cycling.th-herve.fr"
 
 
 # set the language_code TODO: set language selection
@@ -30,45 +36,49 @@ MEN_2023_SEASON_ID = "sr:stage:1023889"
 events = fetch_season_data(MEN_2023_SEASON_ID)
 events = format_season_info(events)
 
-@app.route('/', methods=["GET", "POST"])
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-
     if request.method == "POST":
-        session['events_user_selected'] = request.form.getlist("event_selection_checkbox")
-        return redirect('/authorize')
-
-
+        session["events_user_selected"] = request.form.getlist(
+            "event_selection_checkbox"
+        )
+        return redirect("/authorize")
 
     # if there is a response from google, add info to user calendar
-    if 'code' in request.args:
-        events_user_selected = session['events_user_selected']
+    if "code" in request.args:
+        events_user_selected = session["events_user_selected"]
         authorization_response = request.url
         flow.fetch_token(authorization_response=authorization_response)
 
         credentials = flow.credentials
-        
+
         # add the event to the user's calendar in a separate thread so it does not interrupt the program
-        adding_event_thread = threading.Thread(target=add_event_user_calendar, args=(credentials, events, events_user_selected))
+        adding_event_thread = threading.Thread(
+            target=add_event_user_calendar,
+            args=(credentials, events, events_user_selected),
+        )
         adding_event_thread.start()
 
+    return render_template(
+        "index.html", events=events, retrive_race_data=retrive_race_data
+    )
 
-    return render_template("index.html", events=events, retrive_race_data=retrive_race_data);
 
-
-@app.route('/race_data/<stage_id>')
+@app.route("/race_data/<stage_id>")
 def race_data(stage_id):
     data = retrive_race_data(stage_id)
     return data
 
 
-@app.route('/authorize')
+@app.route("/authorize")
 def authorize():
-    auth_uri, state = flow.authorization_url(access_type='offline', 
-                        include_granted_scopes='true')
+    auth_uri, state = flow.authorization_url(
+        access_type="offline", include_granted_scopes="true"
+    )
 
     return redirect(auth_uri)
 
 
-
 if __name__ == "__main__":
-    app.run(debug=False, ssl_context='adhoc')
+    app.run(debug=False, ssl_context="adhoc")
